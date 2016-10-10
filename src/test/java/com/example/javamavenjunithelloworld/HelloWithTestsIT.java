3b283510f5c3ccc010ce3f9361de29e169d2d15d
vpadegaonkar <vpadegaonkar@yahoo.com>
2016-10-10 19:36:42 +0000

    fixed firefox path

--- src/test/java/com/example/javamavenjunithelloworld/HelloWithTestsIT.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloWithTestsIT.java
@@ -50,7 +50,7 @@ public class HelloWithTestsIT {
         String Xport = System.getProperty(
                 "lmportal.xvfb.id", ":1");
         final File firefoxPath = new File(System.getProperty(
-                "lmportal.deploy.firefox.path", "/firefox"));
+                "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
         FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
         firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
 
