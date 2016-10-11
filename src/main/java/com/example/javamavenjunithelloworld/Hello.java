2de56017c89510938ee2b9e95e9826fa353ba99a
vpadegaonkar <vpadegaonkar@yahoo.com>
2016-10-11 12:27:21 +0000

    Added Barclays

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -34,6 +34,7 @@ public class Hello {
     public void sayHello(PrintStream printer) {
         for (short i = 0; i < times; i++) {
             printer.println(HELLO);
+            System.out.println("Barclays");
         }
     }
 
